fe63ca5a9ea32919f9f9be0cb55db7d9b69d0234
Roma <kurpelroma@gmail.com>
2024-12-03 01:06:11 +0200

    Закінчив верстку для  десктопного варіанту

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
-
+const swiper = new Swiper('.swiper-reviews', {
+    // Optional parameters
+    slidesPerView: 3.3,
+    centeredSlides: true,
+    spaceBetween: 50,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
 
